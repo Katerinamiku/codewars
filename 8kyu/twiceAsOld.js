000619b7630db2acba3d9fff7bc39efe7f6b3c7e
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-01-23 20:02:57 +0300

    add new task

---
+++ 8kyu/twiceAsOld.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5b853229cfde412a470000d0/train/javascript
+function twiceAsOld(dadYearsOld, sonYearsOld) {
+  answer = Math.abs(dadYearsOld - sonYearsOld * 2);
+  return answer;
+}
