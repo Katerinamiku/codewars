02d2b43211a9d66255ed241fbf91f42527ec6b7e
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-01-27 17:06:47 +0300

    add alternative solution

--- 8kyu/arrayPlusArray.js
+++ 8kyu/arrayPlusArray.js
@@ -8,3 +8,8 @@ function arrayPlusArray(arr1, arr2) {
   });
   return total1 + total2;
 }
+
+// Alternative solution
+function arrayPlusArray(arr1, arr2) {
+  return arr1.concat(arr2).reduce((a, b) => a + b);
+}
