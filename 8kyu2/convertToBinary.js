54c569841c9503db800e4d2b60659b5bdd857e9a
Katerina <70574046+Katerinamiku@users.noreply.github.com>
2022-01-28 18:54:53 +0300

    Delete 8kyu2 directory
    deleting mistakes

--- 8kyu2/convertToBinary.js
+++
@@ -1,4 +0,0 @@
-// https://www.codewars.com/kata/59fca81a5712f9fa4700159a/train/javascript
-function toBinary(n) {
-  return parseInt(n.toString(2));
-}
--- 8kyu2/reverswdSequense.js
+++
@@ -1,8 +0,0 @@
-// https://www.codewars.com/kata/5a00e05cc374cb34d100000d/train/javascript
-const reverseSeq = (n) => {
-  let arr = [];
-  for (i = n; i > 0; i--) {
-    arr.push(i);
-  }
-  return arr;
-};
