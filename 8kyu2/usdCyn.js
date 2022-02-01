6fb5880271a2e1d5a486648a433e44097e6eb3f6
Katerinamiku <70574046+Katerinamiku@users.noreply.github.com>
2022-02-01 14:20:45 +0300

    add new task

--- 8kyu2/usdCyn.js
+++ 8kyu2/usdCyn.js
@@ -1,5 +1,5 @@
 // https://www.codewars.com/kata/5977618080ef220766000022/train/javascript
 function usdcny(usd) {
   let cost = usd * 6.75;
-  return cost + " Chinese Yuan";
+  return cost.toFixed(2) + " Chinese Yuan";
 }
